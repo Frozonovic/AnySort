401bbd904d952020422dd0523a3bbc9a7cb5de9b
Brian Snider <bsnider@georgefox.edu>
2019-11-01 10:55:45 -0700

    initial commit

---
+++ anysort.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file anysort.cpp
+ * @brief Parallel (threaded) implementation of the X sort algorithm.
+ *
+ * @author Brian R. Snider (bsnider@georgefox.edu)
+ */
+
+#include <iostream>
+
+/**
+ * @brief Main entry point of the program.
+ *
+ * @return The exit status of the program.
+ */
+int main(int argc, char* argv[]) {
+    // TODO implement program
+
+    return 0;
+}
+
