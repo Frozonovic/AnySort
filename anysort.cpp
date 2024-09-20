891d437178788777230b55677d9f5176aa8f6c5a
glacialfrozone <blee20@georgefox.edu>
2024-09-19 23:28:28 -0700

    Not sure if I went overboard or not, but I wanted mine to have a more "dynamic" feel to it, so we've got some good ol' RNG, dynamic array allocation, and some more fun goodies here. Hope you enjoy!

--- anysort.cpp
+++ anysort.cpp
@@ -14,77 +14,117 @@
  * @return The exit status of the program.
  */
 int main() {
-   // TODO implement program
-   long long *arr = randomized_array();
-   size_t arr_size = sizeof(arr);
-   size_t thread_count = random_thread_count();
-   
-   selection_sort(arr, arr_size);
-   // parallel_selection_sort(thread_cout, arr_size, arr);
-   
-   delete[] arr;
-   return 0;
+    std::tuple<long long *, size_t> arr_and_size = randomized_array();
+    long long *arr = std::get<0>(arr_and_size);
+    size_t arr_size = std::get<1>(arr_and_size);
+    
+    par_selection_sort(arr, arr_size);
+    
+    std::cout << arr_size << '\n' << std::endl;
+    for (int i = 0; i < arr_size; ++i) {
+        std::cout << arr[i] << std::endl;
+    }
+    
+    delete[] arr;
+    return 0;
 }
 
 
 size_t random_thread_count() {
-   std::random_device rand;
-   std::mt19937 gen(rand());
-   std::uniform_int_distribution<> dist(1, 12);
-   
-   return dist(gen);
+    std::random_device rand;
+    std::mt19937 gen(rand());
+    std::uniform_int_distribution<> dist(2, 6);
+    
+    return dist(gen);
 }
 
 
-long long *randomized_array() {
-   std::random_device rand;
-   std::mt19937 gen(rand());
-   std::uniform_int_distribution<> dist(1, 100);
-   
-   size_t arr_size = dist(gen);
-   
-   auto *arr = new long long[arr_size];
-   
-   for (int i = 0; i < arr_size; ++i) {
-      arr[i] = dist(gen);
-   }
-   
-   return arr;
+std::tuple<long long *, size_t> randomized_array() {
+    std::random_device rand;
+    std::mt19937 gen(rand());
+    std::uniform_int_distribution<> dist(1, 100);
+    
+    size_t arr_size = dist(gen);
+    
+    auto *arr = new long long[arr_size];
+    
+    for (int i = 0; i < arr_size; ++i) {
+        arr[i] = dist(gen);
+    }
+    
+    return std::tuple<long long *, size_t>{arr, arr_size};
 }
 
 
-void selection_sort(long long *arr, size_t arr_size) {
-   for (size_t i = 0; i < arr_size - 1; ++i) {
-      size_t min_index = i;
-      for (size_t j = i + 1; j < arr_size; ++j) {
-         if (arr[j] < arr[min_index]) {
-            min_index = j;
-         }
-      }
-      
-      if (min_index != i) {
-         swap(min_index, i, arr);
-      }
-   }
+void par_selection_sort(long long *arr, size_t arr_size) {
+    for (size_t i = 0; i < arr_size - 1; ++i) {
+        size_t unsorted_elements = arr_size - i;
+        size_t second_half = std::floor(unsorted_elements / 4);
+        size_t thread_count = std::min(random_thread_count(), second_half);
+        
+        if (thread_count == 0) {
+            thread_count = 1;
+        }
+        
+        std::thread threads[thread_count];
+        std::tuple<long long, size_t> locals[thread_count];
+        
+        for (size_t j = 0; j < thread_count; ++j) {
+            size_t start = j * (unsorted_elements / thread_count) + i;
+            
+            size_t end;
+            if (j == thread_count - 1) {
+                // Out of bounds safety measure
+                end = arr_size;
+            } else {
+                // Starting index plus the number of elements managed
+                end = start + (unsorted_elements / thread_count);
+            }
+            
+            threads[j] = std::thread(get_local_minima, arr, start, end, std::ref(locals[j]));
+        }
+        
+        for (auto &thread: threads) {
+            thread.join();
+        }
+        
+        global_sort(arr, locals, i, thread_count);
+    }
 }
 
 
-void parallel_selection_sort(long long *arr, size_t arr_size, size_t thread_count) {
-   std::thread threads[thread_count];
-   
-   for (int i = 0; i < thread_count; ++i) {
-      threads[i] = std::thread(i, thread_sort);
-   }
+void get_local_minima(const long long *arr, size_t start, size_t end, std::tuple<long long, size_t> &local) {
+    long long local_min = arr[start];
+    size_t local_min_index = start;
+    
+    for (size_t i = start + 1; i < end; ++i) {
+        if (arr[i] < local_min) {
+            local_min = arr[i];
+            local_min_index = i;
+        }
+    }
+    
+    local = std::tuple<long long, size_t>(local_min, local_min_index);
 }
 
 
-void thread_sort() {
-
+void global_sort(long long *arr, std::tuple<long long, size_t> *locals, size_t global_index, size_t thread_count) {
+    std::tuple<long long, size_t> curr_min = locals[0];
+    
+    for (size_t i = 1; i < thread_count; ++i) {
+        std::tuple<long long, size_t> potential_min = locals[i];
+        
+        if (std::get<0>(potential_min) < std::get<0>(curr_min)) {
+            curr_min = potential_min;
+        }
+    }
+    
+    swap(global_index, std::get<1>(curr_min), arr);
 }
 
 
 void swap(const size_t j, const size_t i, long long *arr) {
-   long long temp_j = arr[j];
-   arr[j] = arr[i];
-   arr[i] = temp_j;
+    long long temp_j = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp_j;
 }
--- anysort.h
+++ anysort.h
@@ -9,37 +9,69 @@
 #define ANYSORT_H
 
 
+#include <algorithm>
 #include <climits>
 #include <cstdlib>
 #include <iostream>
 #include <random>
 #include <thread>
+#include <tuple>
 
 
-struct min {
-   size_t min_index;
-   long long value;
-   bool mutex;
-   
-   min(): min_index(ULONG_LONG_MAX), value(LLONG_MAX), mutex(false) {};
-};
-
-
+/**
+ * @brief Generates a random number of threads to spin off
+ *
+ * @return Random, positive integer
+ */
 size_t random_thread_count();
 
 
-long long* randomized_array();
+/**
+ * @brief Randomizes an array of values
+ *
+ * @return Filled array with size
+ */
+std::tuple<long long*, size_t> randomized_array();
 
 
-void selection_sort(long long *arr, size_t n);
+/**
+ * @brief Parallelized implementation of selection sort
+ *
+ * @param arr Array of values for sorting
+ * @param arr_size Size of the array of values
+ */
+void par_selection_sort(long long *arr, size_t arr_size);
 
 
-void parallel_selection_sort(long long *arr, size_t arr_size, size_t thread_count);
+/**
+ * @brief Gets the local minimum value from a subset of the global array
+ *
+ * @param arr Global array of values
+ * @param start First index of subset
+ * @param end Last index of subset
+ * @param local A paired value with its global array index
+ */
+void get_local_minima(const long long *arr, size_t start, size_t end, std::tuple<long long, size_t> &local);
 
 
-void thread_sort();
+/**
+ * @brief Globally sorts the local minima
+ *
+ * @param arr Global array of values
+ * @param locals All local minima paired values
+ * @param global_index Global index for elements left to sort
+ * @param thread_count Number of threads spun off
+ */
+void global_sort(long long *arr, std::tuple<long long, size_t> *locals, size_t global_index, size_t thread_count);
 
 
-void swap(size_t i, size_t j, long long* arr);
+/**
+ * @brief Takes two elements and switches their place in the array
+ *
+ * @param j Second index of
+ * @param i
+ * @param arr
+ */
+void swap(size_t j, size_t i, long long *arr);
 
 #endif
